f3e51d3e581b39c18a684c7dcb5373b2a24581dd
Ali Eren Öztürk <oztalieren@gmail.com>
2022-05-15 13:46:19 +0300

    feat: add csv export on admin transaction and update transaction task

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.http import HttpResponse
 
 from core.models.organization import (
     Organization,
@@ -9,6 +10,49 @@ from core.models.organization import (
 from core.models.transaction import Transaction, TransactionHandle
 
 
+class TransactionAdmin(admin.ModelAdmin):
+    def download_csv(modeladmin, request, queryset):
+        import csv
+
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = "attachment; filename=transactions.csv"
+
+        writer = csv.writer(response)
+        writer.writerow(
+            [
+                "timestamp",
+                "amount",
+                "transaction_type",
+                "transaction_status",
+                "organization_id",
+                "organization_title",
+            ]
+        )
+        for s in queryset:
+            writer.writerow(
+                [
+                    s.timestamp,
+                    s.amount,
+                    s.transaction_type,
+                    s.transaction_status,
+                    s.organization.pk,
+                    s.organization.title,
+                ]
+            )
+
+        return response
+
+    list_display = (
+        "timestamp",
+        "amount",
+        "transaction_type",
+        "transaction_status",
+        "organization",
+    )
+    list_filter = ("organization",)
+    actions = [download_csv]
+
+
 class OrganizationUserAdmin(admin.ModelAdmin):
     exclude = ("password",)
 
@@ -17,5 +61,5 @@ admin.site.register(Organization)
 admin.site.register(OrganizationUser, OrganizationUserAdmin)
 admin.site.register(OrganizationUserAuthHistory)
 admin.site.register(OrganizationUserPermission)
-admin.site.register(Transaction)
+admin.site.register(Transaction, TransactionAdmin)
 admin.site.register(TransactionHandle)
--- core/tasks.py
+++ core/tasks.py
@@ -2,24 +2,28 @@ from celery import shared_task
 from django.db import DatabaseError, transaction
 
 from core.models.enums.transaction import TransactionStatus, TransactionType
+from core.models.transaction import Organization as OrganizationModel
 from core.models.transaction import Transaction as TransactionModel
 
 
 @shared_task
 def handle_approved_transaction(transaction_id):
 
-    transaction_object = TransactionModel.objects.get(pk=transaction_id)
-    try:
-        with transaction.atomic():
-            organization = transaction_object.organization
-            if transaction_object.transaction_type == TransactionType.DEPOSIT:
-                organization.balance += transaction_object.amount
-            elif transaction_object.transaction_type == TransactionType.WITHDRAWAL:
-                organization.balance -= transaction_object.amount
+    with transaction.atomic():
+        transaction_object = TransactionModel.objects.select_for_update().get(pk=transaction_id, transaction_status=TransactionStatus.PENDING)
+        organization_object = OrganizationModel.objects.select_for_update().get(
+            pk=transaction_object.organization.pk
+        )
+        try:
+            with transaction.atomic():
+                if transaction_object.transaction_type == TransactionType.DEPOSIT:
+                    organization_object.balance += transaction_object.amount
+                elif transaction_object.transaction_type == TransactionType.WITHDRAWAL:
+                    organization_object.balance -= transaction_object.amount
 
-            organization.save()
-            transaction_object.transaction_status = TransactionStatus.COMPLETED
-    except DatabaseError:
-        transaction_object.transaction_status = TransactionStatus.ERROR
+                organization_object.save()
+                transaction_object.transaction_status = TransactionStatus.COMPLETED
+        except DatabaseError:
+            transaction_object.transaction_status = TransactionStatus.ERROR
 
-    transaction_object.save()
+        transaction_object.save()
